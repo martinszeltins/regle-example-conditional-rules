083503265c80387eb8a33d67e7ac3e08fd8daf4e
Martins Zeltins <martins.zeltins@gmail.com>
2024-12-14 12:02:41 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
                 </button>
 
                 <!-- Secondary button to toggle condition -->
-                <button @click="condition = !condition" class="bg-gray-500 text-white rounded py-2 px-5 hover:bg-gray-900 transition hover:active:bg-gray-950 dark:bg-gray-700 dark:hover:bg-gray-800 dark:hover:active:bg-gray-900">
+                <button type="button" @click="condition = !condition" class="bg-gray-500 text-white rounded py-2 px-5 hover:bg-gray-900 transition hover:active:bg-gray-950 dark:bg-gray-700 dark:hover:bg-gray-800 dark:hover:active:bg-gray-900">
                     Toggle Condition
                 </button> <span @click="condition = !condition">{{ condition }}</span>
             </div>
@@ -37,9 +37,9 @@
 
 <script setup lang="ts">
     import { computed, ref } from 'vue'
-    import { required, ruleHelpers } from '@regle/rules'
     import FieldError from './components/FieldError.vue'
-    import { useRegle, createRule, type Maybe, type RegleComputedRules } from '@regle/core'
+    import { applyIf, minLength, required } from '@regle/rules'
+    import { useRegle, type RegleComputedRules } from '@regle/core'
 
     const condition = ref(true)
     const isFormValid = ref(false)
@@ -49,35 +49,20 @@
         lastName: ''
     })
 
-    /**
-     * It is recommended to create custom rules using the `createRule` function
-     * instead of an inline function. It automatically tracks reactive
-     * dependencies and allows you to add custom `active` behavior
-     * to the rule.
-     */
-    const customRule = createRule({
-        validator(value: Maybe<string>, minLength: number, condition: boolean) {
-            return ruleHelpers.isFilled(value) && value.length >= minLength && condition === true
-        },
-
-        message: ({ $params: [minLength] }) => {
-            return `The field must be filled and have more than ${minLength} characters, condition must be true.`
-        }
-    })
-
     const rules = computed(() => {
         return {
             firstName: {
                 required,
-                customRule: customRule(15, condition)
+                minLength: applyIf(condition, minLength(10))
             },
             lastName: {
                 required,
+                minLength: applyIf(condition, minLength(15))
             }
         } satisfies RegleComputedRules<typeof form>
     })
 
-    const { r$ } = useRegle(form, rules, { autoDirty: false })
+    const { r$ } = useRegle(form, rules)
 
     const submit = async () => {
         isFormValid.value = false
